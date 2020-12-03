2dbc037eded6d9d1478180f32d5d0d24a1f695bb
jason1110 <Jason.easterly11@gmail.com>
2020-12-03 10:22:39 -0700

    add create and destroy methods

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,7 +3,26 @@ class TodosController < ApplicationController
     def index
     @todos = Todo.all
 
-    render json: { Todo: @todos}
+    render json: @todos
+    end
+
+    def create
+        @todo = Todo.create(
+            title: params[:title],
+            content: params[:content],
+            urgent: params[:urgent],
+            done: params[:done]
+        )
+
+        render json: @todo, status: :created
+    end
+
+    def destroy
+        @todo = Todo.find(params[:id])
+        
+        @todo.destroy
+
+        render status: :no_content
     end
 
 
